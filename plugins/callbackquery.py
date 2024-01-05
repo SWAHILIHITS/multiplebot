6851b9043166077f6666a153dbb638e4829c521d
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2024-01-05 08:23:22 +0300

    Update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -22,9 +22,9 @@ async def group2(client, message):
         ) 
 @Bot0.on_message( filters.command('rename') & filters.private)
 async def grouop2(client, message):
-    if len(message.command) != 3:
+    if len(message.command) != 5:
         await message.reply_text(
-            f"tuma /rename 1234 1234")
+            f"tuma /rename 1234kb 1234kw usrkbadlisha usrkuweka")
         return 
     raw_pattern = r'\b' + message.command[1] + r'.*'
     try:
@@ -47,8 +47,8 @@ async def grouop2(client, message):
                 continue
             else:
                 strid = str(uuid.uuid4())
-                file.reply=file.reply.replace("Mrlovebite_Tz","Franecemasmovies2013")
-                file.reply=file.reply.replace("hrm45","Franecemasmovies2013")
+                file.reply=file.reply.replace( message.command[3] ,message.command[4])
+                file.reply=file.reply.replace("hrm45",message.command[4])
                 await save_file(file.text.split(".dd#")[0] + ".dd#." + message.command[2], file.reply, file.btn, file.file, file.type, strid, int(message.command[2]) ,file.descp,file.price,file.grp)
         
 @Bot0.on_callback_query()
