1b48c19cadd475051981c6f240c0ff2e7dbab87e
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2024-07-14 15:03:50 +0300

    Update botii.py

--- botii.py
+++ botii.py
@@ -171,8 +171,8 @@ class Bot9(Client):
     async def stop(self, *args):
         await super().stop()
 BOT0=None
-for i in [Bot2,Bot]:
+for i in [Bot2,Bot,Bot1]:
     Bot0=i
 async def main():
-    app=[Bot2(),Bot()]
+    app=[Bot2(),Bot(),Bot1()]
     await compose(app)
