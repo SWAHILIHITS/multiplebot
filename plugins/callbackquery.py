b3d96e8dc83867dfa49e945dd6069c8487e682a9
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2026-03-26 06:09:20 +0300

    Update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -27,7 +27,23 @@ async def index_files(bot, message):
         await message.reply('Wait until previous process complete.')
     else:
         while True:
-            last_msg = await bot.ask(text = "Forward me last message of a channel which I should save to my database.\n\nYou can forward posts from any public channel, but for private channels bot should be an admin in the channel.\n\nMake sure to forward with quotes (Not as a copy)", chat_id = message.from_user.id)
+            lasti ="Forward me last message of a channel which I should save to my database.\n\nYou can forward posts from any public channel, but for private channels bot should be an admin in the channel.\n\nMake sure to forward with quotes (Not as a copy)"
+            mkv11 = await bot.send_message(chat_id = query.from_user.id,text=lasti)
+            a,b = funask()
+            id1 = mkv11.id + 1
+            while a==False:
+                try:
+                    last_msg = await bot.get_messages("me",id1)
+                    if (mkv1.document | mkv1.video)!=None:
+                        a=True
+                    if (time.time()-b)>(3*60):
+                        await bot.send_message(chat_id = message.from_user.id,text=f" Tafadhali anza upya jitahidi kutuma ujumbe ndani ya dakika 3 iliniweze kuhudumia na wengine")
+                        return
+                    if mkv1.from_user.id != message.from_user.id :
+                        a=False
+                        id1=id1+1
+                except:
+                    a=False
             try:
                 last_msg_id = last_msg.forward_from_message_id
                 if last_msg.forward_from_chat.username:
