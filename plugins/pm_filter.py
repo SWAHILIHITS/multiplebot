5cfb8d99d58c6a14c8a3f821aea9268a5a96aaea
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2026-04-06 11:12:08 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -34,7 +34,7 @@ def getCreds():
           creds.refresh(Request())
       else:
           flow = InstalledAppFlow.from_client_secrets_file(
-              'credentials.json', SCOPES)
+              './client_secret.json', SCOPES)
           creds = flow.run_local_server(port=0)
       # Save the credentials for the next run
       with open('token.pickle', 'wb') as token:
