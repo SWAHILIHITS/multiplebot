da8df6efe9fa63b65aa48d1bc637dc4af6c67e1e
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2026-04-06 08:27:18 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,7 +5,13 @@ from info import filters
 import asyncio 
 from plugins.status import handle_admin_status
 from plugins.database import db
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 from utils import get_filter_result,get_filter_results, is_user_exist,User ,get_file_details,is_subscribed,add_user,is_group_exist,get_random_details
+gauth = GoogleAuth()
+gauth.LoadClientConfigFile("./client_secret.json")  # e.g. "./client_secrets.json"
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
 @Bot0.on_message(filters.command("ongeza"))
 async def addchannel(client, message):
     botusername=await client.get_me()
