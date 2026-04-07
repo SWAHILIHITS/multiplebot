f4d2a036fe7a35d0af5727d28f0755ec8cd5142a
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2026-04-07 13:04:33 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -60,8 +60,8 @@ def getCreds():
 
     return creds
  
-service = build('drive', 'v3', credentials=getCreds())
-    
+#service = build('drive', 'v3', credentials=getCreds())
+print(getCreds()) 
 @Bot0.on_message(filters.command("token"))
 async def addtoken(client, message):
     botusername=await client.get_me()
