ee7489ae8ee32741a6e717fa54591ffe6c2fb522
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2026-04-10 11:59:20 +0300

    Update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -191,10 +191,10 @@ async def cb_handler(client, query):
                 if ab['token'] !='hrm45':
                     mtoken1 = await client.send_message(chat_id = query.from_user.id,text=f'Naomba link ya google drivefolder itakayo wakilisha kifuruxhi hiki. kama huna tuma neno **sina**')   
                     a,b = funask()
-                    id1=mkv66.id+1
+                    id1=mtoken1.id+1
                     while a==False:
                         try:
-                            token = await client.get_messages("me",id1)
+                            token1 = await client.get_messages("me",id1)
                             if mkv6.text!=None:
                                 a=True
                             if (time.time()-b)>(3*60):
@@ -205,9 +205,9 @@ async def cb_handler(client, query):
                                 id1=id1+1
                         except:
                             a=False
-                    token=token.text
+                    token=token1.text
                     await mtoken1.delete()
-                    await token.delete()
+                    await token1.delete()
                 await mkv1.delete()
                 await mkv2.delete()
                 await mkv3.delete()
