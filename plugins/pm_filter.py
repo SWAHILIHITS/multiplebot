3ffe7481b84c1bd04413249fc295c27881c6a19d
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2026-04-11 09:10:10 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -469,7 +469,7 @@ async def groupprv(client, message):
                         fvc=str(fvc)
                         if 'user_given_access' not in fvc:
                             text1+=f"{text78}\n"
-                            await client.send_message(chat_id=group_id,text=f'tafadhali hakiki email yake{text.lower()} au link yako kama inafanya kaz nmeshindwa kumuwezesha{text78} link ni {descp.split(".dd#.")[2]}')
+                            await client.send_message(chat_id=group_id,text=f'{fvc}tafadhali hakiki email yake{text.lower()} au link yako kama inafanya kaz nmeshindwa kumuwezesha{text78} link ni {descp.split(".dd#.")[2]}')
                             continue
                         else:
                             cvb="no"
