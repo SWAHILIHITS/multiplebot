ee3354a3d6f568785731852f08919b51be726b58
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2026-04-11 08:42:52 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -464,7 +464,7 @@ async def groupprv(client, message):
                             await client.send_message(chat_id=group_id,text=f'tafadhali token imeexpire tengeneza mpya')
                             continue
                         fvc=grant_access(service, descp.split(".dd#.")[2], text.lower())
-                        if fvc != 'user_given_access':
+                        if 'user_given_access' not in fvc:
                             text1+=f"{text78}\n"
                             await client.send_message(chat_id=group_id,text=f'tafadhali hakiki email yake{text.lower()} au link yako kama inafanya kaz nmeshindwa kumuwezesha{text78} link ni {descp.split(".dd#.")[2]}')
                             continue
