17a5b20f53e1e673cfec303bb73d4fb976be02ba
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2026-04-29 21:53:25 +0300

    Update bpay.py

--- plugins/bpay.py
+++ plugins/bpay.py
@@ -5,13 +5,13 @@ import uuid
 API_URL = "https://zenoapi.com/api/payments/mobile_money_tanzania"
 # Replace with your actual API Key
 API_KEY = "Ca_mt_lI-RMjVDI3N0BSJGYC_FHIhOL6i2eIYA6PavLU36rLUfbKoUtmG5wsF69Z_S2NGiXmUhJWmRVmQKpwxw"
-
+order_id=str(uuid.uuid4())
 payload = {
-    "order_id": str(uuid.uuid4()),    # MANDATORY: Must be a unique UUID
+    "order_id": order_id ,    # MANDATORY: Must be a unique UUID
     "buyer_name": "idd mohamed",        # MANDATORY
     "buyer_email": "hramamogamed@gmail.com", # MANDATORY
     "buyer_phone": "0652470409",      # MANDATORY: 10 digits starting with 0
-    "amount": 10000,                  # MANDATORY: Number, not string
+    "amount": 100,                  # MANDATORY: Number, not string
     #"webhook_url": "https://yourdomain.com" # Recommended
 }
 
